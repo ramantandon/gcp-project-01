4d33dbd077b141c54d56f4abee35a48fbf402ea0
Raman Tandon <rtandon.ny@gmail.com>
2026-02-17 15:59:43 -0500

    Enable kubernetes service

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ resource "random_id" "project_id" {
   byte_length = 3
 }
 
+locals {
+  project = "${var.project}-${random_id.project_id.hex}"
+}
+
 resource "google_project" "project" {
   name                = var.project
-  project_id          = "${var.project}-${random_id.project_id.hex}"
+  project_id          = local.project
   billing_account     = var.billing-account-id
   auto_create_network = "false"
   lifecycle {
