d2a01958f6e0a05433e8b48f9eb5910a19743793
Raman Tandon <rtandon.ny@gmail.com>
2026-02-18 14:51:20 -0500

    provision instance-3

--- instances.tf
+++ instances.tf
@@ -51,3 +51,30 @@ resource "google_compute_instance" "instance-2" {
     scopes = ["cloud-platform"]
   }
 }
+
+resource "google_compute_instance" "instance-3" {
+  name                      = "instance-3"
+  machine_type              = "e2-medium"
+  project                   = local.project
+  zone                      = "us-east1-b"
+  tags                      = ["allow-ssh"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2510-amd64"
+    }
+  }
+
+  network_interface {
+    # Reference the 'self_link' from the specific subnet in the module output
+    subnetwork = module.vpc_network.subnets["us-east1/subnet-us-east1"].self_link
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+}
